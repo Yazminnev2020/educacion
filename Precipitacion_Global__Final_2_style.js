b0d39dd4f317724d6bfbfc3cb62dee686cec3835
Yazminnev2020 <66891565+Yazminnev2020@users.noreply.github.com>
2020-08-21 21:52:53 -0500

    Update Precipitacion_Global__Final_2_style.js

--- Precipitacion_Global__Final_2_style.js
+++ Precipitacion_Global__Final_2_style.js
@@ -1,7 +1,7 @@
 var size = 0;
 var placement = 'point';
 
-var style_Precipitacion_Global__Final_2.png = function(feature, resolution){
+var style_Precipitacion_Global__Final_2 = function(feature, resolution){
     var context = {
         feature: feature,
         variables: {}
